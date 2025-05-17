2c68ceafc632cd78f9661595d594c85d6ece4285
skittery268 <sabadzidzikashvili6@gmail.com>
2025-05-17 21:14:29 +0400

    Initial Commit

--- level 6/homework/hw6.py
+++ level 6/homework/hw6.py
@@ -1,6 +1,6 @@
 # 7) მომხმარებელს შემოატანინეთ წელი და შეამოწმეთ, თუ ის არის ნაკიანი დაბეჭდეთ "This is leap year".(ნაკიანი წელიწადი იყოფა 4-ზე და არ იყოფა 100-ზე ან იყოფა 400-ზე)
 
 year = int(input("Please enter year: "))
-
+ 
 if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-    print("This is leap year")
+    print("This is leap year") 
--- level 7/classwork/cw5.py
+++ level 7/classwork/cw5.py
@@ -1,6 +1,6 @@
 # 5) მომხმარებელს შემოატანინეთ სხვადასხვა სახელები 5-ჯერ იტერაციის მეშვეობით, შექმენით ამ სახელებისთვის ცვლადი რომელშიც ყოველი სახელი დაემატება და ეს სახელები გამოიყოფა მძიმეებით, ანუ თქვენი ცვლადი 5 სახელის შეყვანის შემდეგ უნდა გამოიყურებოდეს ასე: "{სახელი პირველი}, {სახელი მეორე}, ... {სახელი მეხუთე}" 
 
-count = 0
+count = 0 
 
 while count < 5:
     name = input("Please enter name: ") 
@@ -9,4 +9,4 @@ while count < 5:
     name4 = input("Please enter name number 4: ")
     name5 = input("Please enter name number 5: ")
     count = count + 5
-print(f"User names: {name}, {name2}, {name3}, {name4} and {name5}.")
+print(f"User names: {name}, {name2}, {name3}, {name4} and {name5}.") 
