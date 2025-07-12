ef19984eb8982b0d617c8a58e6b9485b0b7e39a5
skittery268 <sabadzidzikashvili6@gmail.com>
2025-07-12 17:22:58 +0400

    Initial Commit

---
+++ level 21/homework/hw4.py
@@ -0,0 +1,10 @@
+# დავალება 4 :
+# დაბეჭდე სტრინგი "Hello, World!" უკუღმა, ყოველი მეორე ასო.
+
+# text = "Hello, World!"
+
+# 📌 შედეგი უნდა იყოს: '!lo olH'
+
+text = "Hello, World!"
+
+print(text[::-2])
---
+++ level 21/homework/hw5.py
@@ -0,0 +1,6 @@
+# დავალება 5:
+# გადახედეთ რესურსებს:
+# https://www.w3schools.com/python/python_strings_slicing.asp
+# https://www.geeksforgeeks.org/python/string-slicing-in-python/
+
+# Done
