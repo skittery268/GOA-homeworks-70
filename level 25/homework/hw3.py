5e15dae07ecd46e657fc51eada9d114de05bd374
skittery268 <sabadzidzikashvili6@gmail.com>
2025-07-21 01:19:17 +0400

    Initial Commit

---
+++ level 25/homework/hw3.py
@@ -0,0 +1,8 @@
+# 3) შექმენით def ფუნქცია სახელად greet - მას უნდა ჰქონდეს 1 არგუმენტი name და  უნდა დაწერე მისალმების ფუნქცია 
+
+user_name = input("Please enter your name: ")
+
+def greet(name):
+    print(f"Hello {name}!")
+
+greet(user_name)
